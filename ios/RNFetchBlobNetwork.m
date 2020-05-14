315f49840e5db82a4a74f7a68f9a4db9b6a05580
Wesley Bercx <wesley.bercx@cusial.com>
2020-05-14 12:08:53 +1000

    Revert networking changes.

--- ios/RNFetchBlobNetwork.m
+++ ios/RNFetchBlobNetwork.m
@@ -13,6 +13,7 @@
 #import "RNFetchBlob.h"
 #import "RNFetchBlobConst.h"
 #import "RNFetchBlobProgress.h"
+
 #if __has_include(<React/RCTAssert.h>)
 #import <React/RCTRootView.h>
 #import <React/RCTLog.h>
@@ -40,34 +41,6 @@ static void initialize_tables() {
     }
 }
 
-
-typedef NS_ENUM(NSUInteger, ResponseFormat) {
-    UTF8,
-    BASE64,
-    AUTO
-};
-
-
-@interface RNFetchBlobNetwork ()
-{
-    BOOL * respFile;
-    BOOL isNewPart;
-    BOOL * isIncrement;
-    NSMutableData * partBuffer;
-    NSString * destPath;
-    NSOutputStream * writeStream;
-    long bodyLength;
-    NSMutableDictionary * respInfo;
-    NSInteger respStatus;
-    NSMutableArray * redirects;
-    ResponseFormat responseFormat;
-    BOOL * followRedirect;
-    BOOL backgroundTask;
-    BOOL uploadTask;
-    BOOL downloadTask;
-}
-
-@end
 
 @implementation RNFetchBlobNetwork
 
@@ -118,109 +91,6 @@ typedef NS_ENUM(NSUInteger, ResponseFormat) {
         [self.requestsTable setObject:request forKey:taskId];
         [self checkProgressConfig];
     }
-
-    backgroundTask = [options valueForKey:@"IOSBackgroundTask"] == nil ? NO : [[options valueForKey:@"IOSBackgroundTask"] boolValue];
-    downloadTask = [options valueForKey:@"IOSDownloadTask"] == nil ? NO : [[options valueForKey:@"IOSDownloadTask"] boolValue];
-    uploadTask = [options valueForKey:@"IOSUploadTask"] == nil ? NO : [[options valueForKey:@"IOSUploadTask"] boolValue];
-
-    NSString * filepath = [options valueForKey:@"uploadFilePath"];
-
-    followRedirect = [options valueForKey:@"followRedirect"] == nil ? YES : [[options valueForKey:@"followRedirect"] boolValue];
-    isIncrement = [options valueForKey:@"increment"] == nil ? NO : [[options valueForKey:@"increment"] boolValue];
-    redirects = [[NSMutableArray alloc] init];
-
-    if(req.URL != nil)
-        [redirects addObject:req.URL.absoluteString];
-
-    // set response format
-    NSString * rnfbResp = [req.allHTTPHeaderFields valueForKey:@"RNFB-Response"];
-    if([[rnfbResp lowercaseString] isEqualToString:@"base64"])
-        responseFormat = BASE64;
-    else if([[rnfbResp lowercaseString] isEqualToString:@"utf8"])
-        responseFormat = UTF8;
-    else
-        responseFormat = AUTO;
-
-    NSString * path = [self.options valueForKey:CONFIG_FILE_PATH];
-    NSString * ext = [self.options valueForKey:CONFIG_FILE_EXT];
-	NSString * key = [self.options valueForKey:CONFIG_KEY];
-    // __block NSURLSession * session;
-
-    bodyLength = contentLength;
-
-    // the session trust any SSL certification
-    NSURLSessionConfiguration *defaultConfigObject;
-
-    defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
-
-    if(backgroundTask)
-    {
-        defaultConfigObject = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:taskId];
-    }
-
-    // set request timeout
-    float timeout = [options valueForKey:@"timeout"] == nil ? -1 : [[options valueForKey:@"timeout"] floatValue];
-    if(timeout > 0)
-    {
-        defaultConfigObject.timeoutIntervalForRequest = timeout/1000;
-    }
-    defaultConfigObject.HTTPMaximumConnectionsPerHost = 10;
-
-    _session = [NSURLSession sessionWithConfiguration:defaultConfigObject delegate:self delegateQueue:taskQueue];
-
-    if(path != nil || [self.options valueForKey:CONFIG_USE_TEMP]!= nil)
-    {
-        respFile = YES;
-
-		NSString* cacheKey = taskId;
-		if (key != nil) {
-            cacheKey = [self md5:key];
-			if (cacheKey == nil) {
-				cacheKey = taskId;
-			}
-
-			destPath = [RNFetchBlobFS getTempPath:cacheKey withExtension:[self.options valueForKey:CONFIG_FILE_EXT]];
-            if ([[NSFileManager defaultManager] fileExistsAtPath:destPath]) {
-				callback(@[[NSNull null], RESP_TYPE_PATH, destPath]);
-                return;
-            }
-		}
-
-        if(path != nil)
-            destPath = path;
-        else
-            destPath = [RNFetchBlobFS getTempPath:cacheKey withExtension:[self.options valueForKey:CONFIG_FILE_EXT]];
-    }
-    else
-    {
-        respData = [[NSMutableData alloc] init];
-        respFile = NO;
-    }
-
-
-    if(uploadTask)
-    {
-        __block NSURLSessionUploadTask * task = [_session uploadTaskWithRequest:req fromFile:[NSURL URLWithString:filepath]];
-        [taskTable setObject:task forKey:taskId];
-        [task resume];
-    }
-    else if(downloadTask)
-    {
-        __block NSURLSessionDownloadTask * task = [_session downloadTaskWithRequest:req];
-        [taskTable setObject:task forKey:taskId];
-        [task resume];
-    }
-    else
-    {
-        __block NSURLSessionDataTask * task = [_session dataTaskWithRequest:req];
-        [taskTable setObject:task forKey:taskId];
-        [task resume];
-    }
-
-    // network status indicator
-    if([[options objectForKey:CONFIG_INDICATOR] boolValue] == YES)
-        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    __block UIApplication * app = [UIApplication sharedApplication];
 }
 
 - (void) checkProgressConfig {
@@ -238,20 +108,6 @@ typedef NS_ENUM(NSUInteger, ResponseFormat) {
 }
 
 - (void) enableProgressReport:(NSString *) taskId config:(RNFetchBlobProgress *)config
-////////////////////////////////////////
-//
-//  NSURLSession delegates
-//
-////////////////////////////////////////
-
-
-#pragma mark NSURLSession delegate methods
-
-
-#pragma mark - Received Response
-
-// set expected content length on response received
-- (void) URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler
 {
     if (config) {
         @synchronized ([RNFetchBlobNetwork class]) {
@@ -261,148 +117,10 @@ typedef NS_ENUM(NSUInteger, ResponseFormat) {
                 [self.requestsTable objectForKey:taskId].progressConfig = config;
             }
         }
-        else
-            respType = @"text";
-            respInfo = @{
-                        @"taskId": taskId,
-                        @"state": @"2",
-                        @"headers": headers,
-                        @"redirects": redirects,
-                        @"respType" : respType,
-                        @"timeout" : @NO,
-                        @"status": [NSNumber numberWithInteger:statusCode]
-                        };
-
-            #pragma mark - handling cookies
-            // # 153 get cookies
-            if(response.URL != nil)
-            {
-                NSHTTPCookieStorage * cookieStore = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-                NSArray<NSHTTPCookie *> * cookies = [NSHTTPCookie cookiesWithResponseHeaderFields: headers forURL:response.URL];
-                if(cookies != nil && [cookies count] > 0) {
-                    [cookieStore setCookies:cookies forURL:response.URL mainDocumentURL:nil];
-                }
-            }
-
-            [self.bridge.eventDispatcher
-            sendDeviceEventWithName: EVENT_STATE_CHANGE
-            body:respInfo
-            ];
-            headers = nil;
-            respInfo = nil;
-
-    }
-    else
-        NSLog(@"oops");
-
-    completionHandler(NSURLSessionResponseAllow);
-}
-
-
-
-// data download progress handler
-- (void) URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
-{
-    // For #143 handling multipart/x-mixed-replace response
-    if(self.isServerPush)
-    {
-        [partBuffer appendData:data];
-        return ;
-    }
-
-    if(respFile == NO)
-    {
-        NSNumber * received = [NSNumber numberWithLong:[data length]];
-        receivedBytes += [received longValue];
-        NSString * chunkString = @"";
-
-        if(isIncrement == YES)
-        {
-            chunkString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        }
-
-        [respData appendData:data];
-
-        RNFetchBlobProgress * pconfig = [progressTable valueForKey:taskId];
-        if(expectedBytes == 0)
-            return;
-        NSNumber * now =[NSNumber numberWithFloat:((float)receivedBytes/(float)expectedBytes)];
-        RCTLog(@"check the didReceiveData ----%f %f %@",(float)receivedBytes,(float)expectedBytes,taskId);
-        if(pconfig != nil && [pconfig shouldReport:now])
-        {
-            [self.bridge.eventDispatcher
-             sendDeviceEventWithName:EVENT_PROGRESS
-             body:@{
-                    @"taskId": taskId,
-                    @"written": [NSString stringWithFormat:@"%d", receivedBytes],
-                    @"total": [NSString stringWithFormat:@"%d", expectedBytes],
-                    @"chunk": chunkString
-                    }
-             ];
-        }
-        received = nil;
-    }
-}
-#pragma mark -
-#pragma mark NSURLSessionDownloadTask delegate methods
-#pragma mark -
-
-  #pragma mark - download progress handler
-
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
-      didWriteData:(int64_t)bytesWritten
- totalBytesWritten:(int64_t)totalBytesWritten
-totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
-
-        RNFetchBlobProgress * pconfig = [progressTable valueForKey:taskId];
-        if(totalBytesWritten == 0)
-            return;
-        NSNumber * now =[NSNumber numberWithFloat:((double)bytesWritten/(double)totalBytesWritten)];
-        RCTLog(@"check the now ----%f %f %@",(double)bytesWritten,(double)totalBytesWritten,taskId);
-        if(pconfig != nil && [pconfig shouldReport:now])
-        {
-            [self.bridge.eventDispatcher
-             sendDeviceEventWithName:EVENT_PROGRESS
-             body:@{
-                    @"taskId": taskId,
-                    @"written": [NSString stringWithFormat:@"%d", totalBytesWritten],
-                    @"total": [NSString stringWithFormat:@"%d", totalBytesExpectedToWrite]
-                    }
-             ];
-        }
-}
-
- #pragma mark - handling didFinishDownloadingToURL
-
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
-didFinishDownloadingToURL:(NSURL *)location {
-    @try{
-        NSLog(@"file path : %@", destPath);
-        if ([[NSFileManager defaultManager] fileExistsAtPath:destPath]) {
-            //Remove the old file from directory
-            [[NSFileManager defaultManager] removeItemAtPath:destPath error:NULL];
-
-        }
-        NSError *error;
-        NSURL *documentURL = [NSURL fileURLWithPath:destPath];
-
-        [[NSFileManager defaultManager] moveItemAtURL:location
-                                                toURL:documentURL
-                                                error:&error];
-        if (!error){
-            //Handle error here
-        }
-    }
-    @catch(NSException * ex)
-    {
-        NSLog(@"write file error");
     }
 }
 
 - (void) enableUploadProgress:(NSString *) taskId config:(RNFetchBlobProgress *)config
-
-# pragma mark - Complete and Error callback
-- (void) URLSession:(NSURLSession *)session didBecomeInvalidWithError:(nullable NSError *)error
 {
     if (config) {
         @synchronized ([RNFetchBlobNetwork class]) {
@@ -441,27 +159,6 @@ didFinishDownloadingToURL:(NSURL *)location {
 
 // #115 Invoke fetch.expire event on those expired requests so that the expired event can be handled
 + (void) emitExpiredTasks
-}
-
-
-
-
-   #pragma mark - Authentication methods
-
-- (void) URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential * _Nullable credantial))completionHandler
-{
-    BOOL trusty = [options valueForKey:CONFIG_TRUSTY];
-    if(!trusty)
-    {
-        completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
-    }
-    else
-    {
-        completionHandler(NSURLSessionAuthChallengeUseCredential, [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
-    }
-}
-
-- (void) URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task willPerformHTTPRedirection:(NSHTTPURLResponse *)response newRequest:(NSURLRequest *)request completionHandler:(void (^)(NSURLRequest * _Nullable))completionHandler
 {
     @synchronized ([RNFetchBlobNetwork class]){
         NSEnumerator * emu =  [expirationTable keyEnumerator];
@@ -480,6 +177,5 @@ didFinishDownloadingToURL:(NSURL *)location {
         expirationTable = [[NSMapTable alloc] init];
     }
 }
-
 
 @end
